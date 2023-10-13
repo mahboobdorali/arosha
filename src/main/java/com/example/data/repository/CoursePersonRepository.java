a78d039e942e87bdaa028d4346da6999c8598102
mahboobdorali <mahboobdorali@gmail.com>
2023-10-13 13:53:33 +0330

    add method for find CoursePerson by courseId

--- src/main/java/com/example/data/repository/CoursePersonRepository.java
+++ src/main/java/com/example/data/repository/CoursePersonRepository.java
@@ -3,8 +3,9 @@ package com.example.data.repository;
 import com.example.data.model.CoursePerson;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
-public interface CoursePersonRepository extends ReactiveCrudRepository<CoursePerson,Long> {
-
+public interface CoursePersonRepository extends ReactiveCrudRepository<CoursePerson, Long> {
+    Flux<CoursePerson> findByCourseId(Long courseId);
 }
