76ff09f3dfee4f37ddc41e59c101fb14729445c7
mahboobdorali <mahboobdorali@gmail.com>
2023-10-13 13:53:53 +0330

    add method for find CoursePerson by courseId in service

--- src/main/java/com/example/service/CoursePersonService.java
+++ src/main/java/com/example/service/CoursePersonService.java
@@ -1,8 +1,12 @@
 package com.example.service;
 
 import com.example.data.dto.CoursePersonDto;
+import com.example.data.model.CoursePerson;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface CoursePersonService {
-    Mono<Void> addPersonToCourse(CoursePersonDto coursePersonDto);
+    Mono<Boolean> addPersonToCourse(CoursePersonDto coursePersonDto);
+    Flux<CoursePerson> findByCourseId(Long courseId);
+
 }
